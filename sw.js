adfb220da2c2aa8f4bbd6fd579cd446762b3dc9d
Ak RIYAD <akriyad33099@gmail.com>
2026-09-21 18:20:33 +0400

    Refactor service worker for better caching and activation
    Implement improved caching strategy for fetch events.

--- sw.js
+++ sw.js
@@ -1,13 +1,34 @@
+const cacheName = 'wifi-setup-v10';
+const assets = ['./', './index.html', './manifest.json', './icon.png'];
+
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
   );
 });
